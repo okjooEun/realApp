a01deffe32a091a6408405885b076d2cb0534bf1
okjooEun <dhrwn5356@swu.ac.kr>
2019-11-29 22:51:20 +0900

    db

--- app/src/main/java/com/example/dbtest/MainActivity.java
+++ app/src/main/java/com/example/dbtest/MainActivity.java
@@ -15,44 +15,28 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import org.w3c.dom.Text;
+
 import java.util.Random;
 
-public class MainActivity extends AppCompatActivity  {
+public class MainActivity extends AppCompatActivity {
 
     ImageView imgCalender, imgTime, guest;
-    Button btnSetting, btnselect1, btnselect2, btnselect3;
-    TextView txttalk;
-    LinearLayout linbtn;
-    boolean isVisible = true;
+    Button btnSetting, btnSelect1, btnSelect2, btnSelect3,goKitchen;
+    LinearLayout linBtn;
+    TextView txtTalk;
+    FrameLayout frame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        imgCalender = (ImageView) findViewById(R.id.imgCalendar);
-        imgTime = (ImageView) findViewById(R.id.imgTime);
-        guest = (ImageView) findViewById(R.id.guest);
-
-        txttalk = (TextView) findViewById(R.id.txttalk);
-        linbtn = (LinearLayout) findViewById(R.id.linBtn);
-
-        btnSetting = (Button) findViewById(R.id.btnSetting);
-        btnselect1 = (Button) findViewById(R.id.btnselect1);
-        btnselect2 = (Button) findViewById(R.id.btnselect2);
-        btnselect3 = (Button) findViewById(R.id.btnselect3);
-
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-
-      /*  //화면 가로 고정
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-*/
         //소프트키(네비게이션바) 없애기 시작
         View decorView = getWindow().getDecorView();
 
@@ -67,13 +51,61 @@ public class MainActivity extends AppCompatActivity  {
 
         decorView.setSystemUiVisibility(uiOption);
         //소프트키(네비게이션바) 없애기 끝
-    }
 
-    public void onClick(View v) {
+        imgCalender = (ImageView) findViewById(R.id.imgCalendar);
+        imgTime = (ImageView) findViewById(R.id.imgTime);
+        guest = (ImageView) findViewById(R.id.guest);
 
-        Intent intent = new Intent(MainActivity.this, TalkActivity.class);
-        startActivity(intent);
+        linBtn = (LinearLayout) findViewById(R.id.linBtn);
+        frame = (FrameLayout) findViewById(R.id.frame);
 
+        txtTalk = (TextView) findViewById(R.id.txTtalk);
 
+        btnSetting = (Button) findViewById(R.id.btnSetting);
+        btnSelect1 = (Button) findViewById(R.id.btnSelect1);
+        btnSelect2 = (Button) findViewById(R.id.btnSelect2);
+        btnSelect3 = (Button) findViewById(R.id.btnSelect3);
+        goKitchen = (Button) findViewById(R.id.goKitchen);
+
+       frame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                txtTalk.setVisibility(View.INVISIBLE);
+                linBtn.setVisibility(View.VISIBLE);
+            }
+        });
+
+        btnSelect1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               linBtn.setVisibility(View.INVISIBLE);
+               txtTalk.setVisibility(View.VISIBLE);
+               goKitchen.setVisibility(View.VISIBLE);
+            }
+        });
+        btnSelect2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                linBtn.setVisibility(View.INVISIBLE);
+                txtTalk.setVisibility(View.VISIBLE);
+                goKitchen.setVisibility(View.VISIBLE);
+            }
+        });
+        btnSelect3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                linBtn.setVisibility(View.INVISIBLE);
+                txtTalk.setVisibility(View.VISIBLE);
+                goKitchen.setVisibility(View.VISIBLE);
+
+            }
+        });
+        goKitchen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, KitchenActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
